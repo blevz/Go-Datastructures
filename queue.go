36ee9d3aecac31898a6459d0412b6a956b691e5b
Ben Levy <bjlgds@gmail.com>
2014-01-24 13:00:16 -0500

    added print and empty to Stack and Queue

--- queue.go
+++ queue.go
@@ -1,5 +1,7 @@
 package ds
 
+import "fmt"
+
 type Queue struct {
 	data []interface{}
 }
@@ -21,10 +23,21 @@ func (q Queue) Size() int {
 	return len(q.data)
 }
 
+func (q Queue) Empty() bool {
+	return len(q.data) == 0
+}
+
 func (q *Queue) Pop() {
 	q.data = q.data[1:q.Size()]
 }
 
 func (q *Queue) Push(x interface{}) {
 	q.data = append(q.data, x)
+}
+
+func (q Queue) Print() {
+	for _, x := range q.data {
+		fmt.Print(x, " ")
+	}
+	fmt.Println("")
 }
--- stack.go
+++ stack.go
@@ -1,5 +1,9 @@
 package ds
 
+import (
+	"fmt"
+)
+
 type Stack struct {
 	data []interface{}
 }
@@ -21,10 +25,21 @@ func (s Stack) Size() int {
 	return len(s.data)
 }
 
+func (s Stack) Empty() bool {
+	return len(s.data) == 0
+}
+
 func (s *Stack) Pop() {
 	s.data = s.data[0 : s.Size()-1]
 }
 
 func (s *Stack) Push(x interface{}) {
 	s.data = append(s.data, x)
+}
+
+func (s Stack) Print() {
+	for _, x := range s.data {
+		fmt.Print(x, " ")
+	}
+	fmt.Println("")
 }
