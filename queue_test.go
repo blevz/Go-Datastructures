a9eb2915e2c169e76e09710e09241939372dbee0
Ben Levy <bjlgds@gmail.com>
2014-07-14 21:46:30 -0400

    better queue tests

--- queue_test.go
+++ queue_test.go
@@ -94,6 +94,26 @@ func Test_Circ_Queue(t *testing.T) {
 	})
 }
 
+func BenchmarkQueuePush(b *testing.B) {
+	var a Queue_I
+	a = MakeQueue()
+	for i := 0; i < b.N; i++ {
+		a.Push(i)
+	}
+}
+
+func BenchmarkQueuePop(b *testing.B) {
+	var a Queue_I
+	a = MakeQueue()
+	for i := 0; i < b.N; i++ {
+		a.Push(i)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		a.Pop()
+	}
+}
+
 func BenchmarkQueuePushAndPop(b *testing.B) {
 	var a Queue_I
 	a = MakeQueue()
@@ -110,6 +130,26 @@ func BenchmarkQueuePushAndPop(b *testing.B) {
 		a.Pop()
 	}
 
+}
+
+func BenchmarkCircQueuePush(b *testing.B) {
+	var a Queue_I
+	a = MakeCircleQueue()
+	for i := 0; i < b.N; i++ {
+		a.Push(i)
+	}
+}
+
+func BenchmarkCircQueuePop(b *testing.B) {
+	var a Queue_I
+	a = MakeCircleQueue()
+	for i := 0; i < b.N; i++ {
+		a.Push(i)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		a.Pop()
+	}
 }
 
 func BenchmarkCircleQueuePushAndPop(b *testing.B) {
